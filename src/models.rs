700e9be524fe5852dbf8b69612d355206abf9989
helltf <Dario.Zachow5530@gmail.com>
2022-05-22 14:18:24 +0200

    feat: model for channel

---
+++ src/models.rs
@@ -0,0 +1,10 @@
+#[derive(Queryable)]
+pub struct ChannelEntity {
+    pub id: i32,
+    pub channel_name: String,
+    pub allowed: bool,
+    pub allowed_live: bool,
+    pub joined: bool,
+    pub times_connected: i32,
+    pub connect_timestamp:i64,
+}
