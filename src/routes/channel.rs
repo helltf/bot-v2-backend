961e8edfb3b8e2c21213617f23906b649161ca60
helltf <Dario.Zachow5530@gmail.com>
2022-07-14 18:05:57 +0200

    feat: resetting everything

--- src/routes/channel.rs
+++
@@ -1,9 +0,0 @@
-use crate::models::channel::ChannelEntity;
-use diesel::PgConnection;
-use rocket::{get, response::status, serde::json::Json};
-
-#[get("/channels")]
-pub fn get_all_channels(connection: &PgConnection) -> Json<Vec<ChannelEntity>> {
-    let data = ChannelEntity::read(connection);
-    return data;
-}
