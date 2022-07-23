c3531142e2d0a3867ac39326a2bc3eccedfd1990
helltf <Dario.Zachow5530@gmail.com>
2022-07-23 14:12:36 +0200

    feat: adding endpoints for posting access token

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,40 @@ extern crate rocket;
 
 #[macro_use]
 extern crate diesel;
+extern crate reqwest;
 
-mod commands_service;
+mod api;
+mod cors;
 mod db;
 mod models;
 mod schema;
+mod services;
+use cors::CORS;
+use rocket::{http::Status, response::status, serde::json::Json};
+use serde::Deserialize;
+use services::commands_service;
 
-use rocket::serde::json::Json;
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+struct AccessToken {
+    pub token: String,
+}
 
 #[get("/commands")]
-fn get_commands() -> Json<Vec<models::CommandEntity>> {
+async fn get_commands() -> Json<Vec<models::CommandEntity>> {
+    let a = api::twitch::get_twitch_id("abc".to_string()).await;
     Json(commands_service::get_commands())
 }
 
+#[post("/token", format = "json", data = "<access_token>")]
+fn post_token(access_token: Json<AccessToken>) -> Json<bool> {
+    return Json(true);
+}
+#[options("/<_..>")]
+fn all_options() {}
+
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![get_commands])
+    rocket::build()
+        .attach(CORS)
+        .mount("/", routes![get_commands, post_token, all_options])
 }
