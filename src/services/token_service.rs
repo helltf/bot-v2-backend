9a7057f013e47088df3741ed4e10137eee1a0a4d
helltf <Dario.Zachow5530@gmail.com>
2022-07-27 11:00:30 +0200

    feat: saving token encrypted with key

--- src/services/token_service.rs
+++ src/services/token_service.rs
@@ -1,14 +1,16 @@
 use crate::schema::twitch_tokens::dsl::*;
 use crate::{db, models::TwitchTokenEntity};
 use diesel::prelude::*;
+use dotenv;
 use sodiumoxide::crypto::secretbox;
-use sodiumoxide::crypto::secretbox::Key;
+use sodiumoxide::crypto::secretbox::xsalsa20poly1305::Key;
 use sodiumoxide::crypto::secretbox::Nonce;
 use std::env;
 
 pub fn save_token(access_token: String, save_refresh_token: String, twitch_id: String) -> () {
     let connection = db::create_connection();
     let box_nonce = secretbox::gen_nonce();
+
     let boxed_at = encrypt_token(access_token, &box_nonce);
 
     let result = diesel::insert_into(twitch_tokens)
@@ -21,7 +23,10 @@ pub fn save_token(access_token: String, save_refresh_token: String, twitch_id: S
 }
 
 fn encrypt_token(enc_token: String, box_nonce: &Nonce) -> Vec<u8> {
-    let key = env::var("BOX_KEY").expect("Key for encrypting is required");
-    Key(key.bytes());
-    return secretbox::seal(enc_token.as_bytes(), &box_nonce, &key);
+    let key_string = env::var("BOX_KEY").expect("Key for encrypting is required");
+    let key_bytes = key_string.as_bytes();
+
+    let key = Key::from_slice(key_bytes).unwrap();
+
+    return secretbox::seal(enc_token.as_bytes(), box_nonce, &key);
 }
