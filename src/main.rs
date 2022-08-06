a4f14fd5c5429b5a9416663aff39e069e6801c00
helltf <Dario.Zachow5530@gmail.com>
2022-08-06 20:33:03 +0200

    refactor(util): moving util function from main into seperate folder

--- src/main.rs
+++ src/main.rs
@@ -24,12 +24,6 @@ pub struct TokenData {
     pub refresh_token: String,
 }
 
-pub fn bytes_to_vec(b: &[u8]) -> Vec<u8> {
-    let mut out = Vec::with_capacity(b.len());
-
-    out.extend_from_slice(b);
-    out
-}
 
 
 #[post("/token", format = "json", data = "<data>")]
