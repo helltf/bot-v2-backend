09cb705286e2f0cb28ebbb3d5c4d8dace9f5f4f3
helltf <Dario.Zachow5530@gmail.com>
2022-08-06 20:33:23 +0200

    refactor(token): move token endpoint into seperate folder

--- src/main.rs
+++ src/main.rs
@@ -11,28 +11,18 @@ mod api;
 mod cors;
 mod db;
 mod models;
+mod routes;
 mod schema;
 mod services;
+mod util;
+
 use cors::CORS;
 use rocket::serde::json::Json;
+use routes::commands::get_commands;
+use routes::token::post_token;
 use serde::Deserialize;
 use services::{commands_service, token_service::save_token};
 
-#[derive(Debug, PartialEq, Eq, Deserialize)]
-pub struct TokenData {
-    pub token: String,
-    pub refresh_token: String,
-}
-
-
-
-#[post("/token", format = "json", data = "<data>")]
-async fn post_token(data: Json<TokenData>) -> Json<bool> {
-    let id = api::twitch::get_twitch_id(data.token.to_owned()).await;
-    save_token(data.token.to_owned(), data.refresh_token.to_owned(), id);
-
-    return Json(true);
-}
 #[options("/<_..>")]
 fn all_options() {}
 
