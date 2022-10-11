da68bee58abf0b98e66af54c2b72e50d7106ecdc
helltf <dario.zachow5530@gmail.com>
2022-10-11 14:30:30 +0200

    feat(token): move getting of access_token to backend

--- src/db.rs
+++ src/db.rs
@@ -1,10 +1,8 @@
 use diesel::pg::PgConnection;
 use diesel::prelude::*;
-use dotenv::dotenv;
 use std::env;
 
 pub fn create_connection() -> PgConnection {
-    dotenv().ok();
     let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     PgConnection::establish(&database_url).expect(&format!("Error connecting to {}", database_url))
 }
--- src/routes/token.rs
+++ src/routes/token.rs
@@ -1,18 +1,25 @@
 use crate::api;
+use crate::api::twitch::{get_token, get_twitch_id};
 use crate::rocket::serde::json::Json;
 use crate::services::token_service::save_token;
 use serde::Deserialize;
-#[derive(Debug, PartialEq, Eq, Deserialize)]
 
-pub struct TokenData {
-    pub token: String,
-    pub refresh_token: String,
+#[derive(Debug, PartialEq, Eq, Deserialize)]
+pub struct Code {
+    pub code: String,
 }
 
 #[post("/token", format = "json", data = "<data>")]
-pub async fn post_token(data: Json<TokenData>) -> Json<bool> {
-    let id = api::twitch::get_twitch_id(data.token.to_owned()).await;
-    save_token(data.token.to_owned(), data.refresh_token.to_owned(), id);
+pub async fn post_token(data: Json<Code>) -> Json<bool> {
+    let token = get_token(data.code.to_owned()).await;
+    let id = get_twitch_id(&token).await;
+
+    let error = save_token(token, id);
+
+    match error {
+        Some(val) => panic!("{}", val),
+        None => (),
+    }
 
     return Json(true);
 }
